33af473f7c276d37e4bd5a70e49002e4800f982a
Alexander <xanderyst@gmail.com>
2016-11-20 10:53:54 -0500

    Added error checking

--- js/angular-router.js
+++ js/angular-router.js
@@ -55,7 +55,11 @@
         console.log(diasterType);
 				totalNeeds = data; // response data 
 				console.log(totalNeeds);
-			});
+			}
+      .error(function(data){
+        console.log("Error: " + $scope.crisis.disaster);
+      });
+
 		}
 	});
 
