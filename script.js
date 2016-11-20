179e273d7118b036f0080e513851622bc8afa179
Brian Zhou <blz4@duke.edu>
2016-11-19 20:32:19 -0500

    Testing map stuff

---
+++ script.js
@@ -0,0 +1,122 @@
+var votingData = {'WA': {'tippingPower': '0.4%', 'votingPowerIndex': '0.2', 'fillKey': '0.2'}, 'DE': {'tippingPower': '0.6%', 'votingPowerIndex': '1.7', 'fillKey': '1.7'}, 'WI': {'tippingPower': '5.7%', 'votingPowerIndex': '2.5', 'fillKey': '2.5'}, 'WV': {'tippingPower': '0.1%', 'votingPowerIndex': '0.1', 'fillKey': '0.1'}, 'HI': {'tippingPower': '0.1%', 'votingPowerIndex': '0.2', 'fillKey': '0.2'}, 'FL': {'tippingPower': '17.0%', 'votingPowerIndex': '2.5', 'fillKey': '2.5'}, 'WY': {'tippingPower': '0.1%', 'votingPowerIndex': '0.1', 'fillKey': '0.1'}, 'NH': {'tippingPower': '2.6%', 'votingPowerIndex': '4.8', 'fillKey': '4.8'}, 'NJ': {'tippingPower': '2.0%', 'votingPowerIndex': '0.7', 'fillKey': '0.7'}, 'NM': {'tippingPower': '1.4%', 'votingPowerIndex': '2.4', 'fillKey': '2.4'}, 'TX': {'tippingPower': '0.8%', 'votingPowerIndex': '0.1', 'fillKey': '0.1'}, 'LA': {'tippingPower': '0.1%', 'votingPowerIndex': '0.1', 'fillKey': '0.1'}, 'NC': {'tippingPower': '9.2%', 'votingPowerIndex': '2.6', 'fillKey': '2.6'}, 'ND': {'tippingPower': '0.1%', 'votingPowerIndex': '0.2', 'fillKey': '0.2'}, 'NE': {'tippingPower': '0.1%', 'votingPowerIndex': '0.1', 'fillKey': '0.1'}, 'TN': {'tippingPower': '0.1%', 'votingPowerIndex': '0.1', 'fillKey': '0.1'}, 'NY': {'tippingPower': '0.1%', 'votingPowerIndex': '0.1', 'fillKey': '0.1'}, 'PA': {'tippingPower': '11.2%', 'votingPowerIndex': '2.6', 'fillKey': '2.6'}, 'RI': {'tippingPower': '0.8%', 'votingPowerIndex': '2.3', 'fillKey': '2.3'}, 'NV': {'tippingPower': '3.7%', 'votingPowerIndex': '4.5', 'fillKey': '4.5'}, 'VA': {'tippingPower': '5.3%', 'votingPowerIndex': '1.8', 'fillKey': '1.8'}, 'CO': {'tippingPower': '6.1%', 'votingPowerIndex': '2.9', 'fillKey': '2.9'}, 'AK': {'tippingPower': '0.3%', 'votingPowerIndex': '1.2', 'fillKey': '1.2'}, 'AL': {'tippingPower': '0.1%', 'votingPowerIndex': '0.1', 'fillKey': '0.1'}, 'AR': {'tippingPower': '0.1%', 'votingPowerIndex': '0.1', 'fillKey': '0.1'}, 'VT': {'tippingPower': '0.2%', 'votingPowerIndex': '0.7', 'fillKey': '0.7'}, 'IL': {'tippingPower': '0.8%', 'votingPowerIndex': '0.2', 'fillKey': '0.2'}, 'GA': {'tippingPower': '2.3%', 'votingPowerIndex': '0.8', 'fillKey': '0.8'}, 'IN': {'tippingPower': '0.1%', 'votingPowerIndex': '0.1', 'fillKey': '0.1'}, 'IA': {'tippingPower': '1.9%', 'votingPowerIndex': '1.6', 'fillKey': '1.6'}, 'OK': {'tippingPower': '0.1%', 'votingPowerIndex': '0.1', 'fillKey': '0.1'}, 'AZ': {'tippingPower': '1.9%', 'votingPowerIndex': '1.0', 'fillKey': '1.0'}, 'CA': {'tippingPower': '0.1%', 'votingPowerIndex': '0.1', 'fillKey': '0.1'}, 'ID': {'tippingPower': '0.1%', 'votingPowerIndex': '0.1', 'fillKey': '0.1'}, 'CT': {'tippingPower': '0.3%', 'votingPowerIndex': '0.3', 'fillKey': '0.3'}, 'ME': {'tippingPower': '0.1%', 'votingPowerIndex': '0.3', 'fillKey': '0.3'}, 'MD': {'tippingPower': '0.1%', 'votingPowerIndex': '0.1', 'fillKey': '0.1'}, 'MA': {'tippingPower': '0.1%', 'votingPowerIndex': '0.1', 'fillKey': '0.1'}, 'OH': {'tippingPower': '9.0%', 'votingPowerIndex': '2.1', 'fillKey': '2.1'}, 'UT': {'tippingPower': '0.1%', 'votingPowerIndex': '0.1', 'fillKey': '0.1'}, 'MO': {'tippingPower': '0.2%', 'votingPowerIndex': '0.1', 'fillKey': '0.1'}, 'MN': {'tippingPower': '3.3%', 'votingPowerIndex': '1.5', 'fillKey': '1.5'}, 'MI': {'tippingPower': '10.0%', 'votingPowerIndex': '2.8', 'fillKey': '2.8'}, 'KS': {'tippingPower': '0.1%', 'votingPowerIndex': '0.1', 'fillKey': '0.1'}, 'MT': {'tippingPower': '0.2%', 'votingPowerIndex': '0.6', 'fillKey': '0.6'}, 'MS': {'tippingPower': '0.1%', 'votingPowerIndex': '0.1', 'fillKey': '0.1'}, 'SC': {'tippingPower': '0.2%', 'votingPowerIndex': '0.1', 'fillKey': '0.1'}, 'KY': {'tippingPower': '0.1%', 'votingPowerIndex': '0.1', 'fillKey': '0.1'}, 'OR': {'tippingPower': '0.7%', 'votingPowerIndex': '0.5', 'fillKey': '0.5'}, 'SD': {'tippingPower': '0.1%', 'votingPowerIndex': '0.3', 'fillKey': '0.3'}};
+var colorKey = {'1.5': '#d1ddcc', '1.6': '#c7d6c1', '1.7': '#c7d6c1', '1.0': '#d1ddcc', '1.2': '#d1ddcc', '0.8': '#dae3d6', '0.1': '#eef2ec', '0.3': '#ebf0e9', '0.2': '#eef2ec', '0.5': '#e3eae0', '0.7': '#dae3d6', '0.6': '#e3eae0', '2.9': '#759a66', '2.8': '#759a66', '2.3': '#acc1a3', '2.1': '#b5c8ad', '2.6': '#759a66', '2.5': '#8cab7f', '2.4': '#a3bb99', '4.5': '#306719', '4.8': '#1a5700', '1.8': '#c7d6c1'}
+/*
+{ '0.9': '#e8d3ff', '1.5': '#e5cdff', '1.7': '#e2c7ff', '1.0': '#e5cdff', '0.1': '#f8f2ff', '0.3': '#f5ecff', '0.2': '#f8f2ff', '0.5': '#ebd9ff', '1.9': '#dfc1ff', '0.7': '#e8d3ff', '0.6': '#ebd9ff', '3.1': '#a64cff', '2.3': '#d8b2ff', '2.2': '#dbb9ff', '2.1': '#dbb9ff', '2.6': '#b266ff', '2.5': '#bf7fff', '2.4': '#cc99ff', '4.5': '#8c19ff', '4.8': '#8000ff', '1.8': '#e2c7ff', '0.4': '#f2e6ff' }
+*/
+var us_state_abbrev = {
+    'alabama': 'AL',
+    'alaska': 'AK',
+    'arizona': 'AZ',
+    'arkansas': 'AR',
+    'california': 'CA',
+    'colorado': 'CO',
+    'connecticut': 'CT',
+    'delaware': 'DE',
+    'florida': 'FL',
+    'georgia': 'GA',
+    'hawaii': 'HI',
+    'idaho': 'ID',
+    'illinois': 'IL',
+    'indiana': 'IN',
+    'iowa': 'IA',
+    'kansas': 'KS',
+    'kentucky': 'KY',
+    'louisiana': 'LA',
+    'maine': 'ME',
+    'maryland': 'MD',
+    'massachusetts': 'MA',
+    'michigan': 'MI',
+    'minnesota': 'MN',
+    'mississippi': 'MS',
+    'missouri': 'MO',
+    'montana': 'MT',
+    'nebraska': 'NE',
+    'nevada': 'NV',
+    'new hampshire': 'NH',
+    'new jersey': 'NJ',
+    'new mexico': 'NM',
+    'new york': 'NY',
+    'north carolina': 'NC',
+    'north dakota': 'ND',
+    'ohio': 'OH',
+    'oklahoma': 'OK',
+    'oregon': 'OR',
+    'pennsylvania': 'PA',
+    'rhode island': 'RI',
+    'south carolina': 'SC',
+    'south dakota': 'SD',
+    'tennessee': 'TN',
+    'texas': 'TX',
+    'utah': 'UT',
+    'vermont': 'VT',
+    'virginia': 'VA',
+    'washington': 'WA',
+    'west virginia': 'WV',
+    'wisconsin': 'WI',
+    'wyoming': 'WY',
+}
+
+var NC_Scores = votingData["NC"];
+
+$("#myState").keyup(function(event) {
+    if (event.keyCode == 13) {
+        $("#submitbutton").click();
+    }
+});
+
+function getStateToVoteIn() {
+    $("#stateToVoteIn").html("");
+    var stateScores;
+    var stateAbbrev;
+    var state = $("#myState").val().trim();
+    if (state.length > 2) {
+        stateAbbrev = us_state_abbrev[state.toLowerCase()];
+        if (stateAbbrev) {
+            stateScores = votingData[stateAbbrev];
+        }
+    } else {
+        stateAbbrev = state.toUpperCase();
+        stateScores = votingData[stateAbbrev];
+    }
+    if (parseFloat(stateScores.votingPowerIndex) >= parseFloat(NC_Scores.votingPowerIndex)) {
+        createVoteLocationResult(stateAbbrev);
+    } else {
+        createVoteLocationResult("NC");
+    }
+}
+
+function createVoteLocationResult(state) {
+    $("#stateToVoteIn").html("");
+    /*
+    //hover over state
+    console.log($(".datamaps-subunit."+state).first().mouseenter())
+    $(".datamaps-hoverover").first().css('display','block');
+    $(".datamaps-subunit."+state).trigger("hover");
+    */
+    var image = "";
+    if (state == "NC") {
+        image = "<img src='DukeVotes.png'>"
+        state = '<a href="https://studentaffairs.duke.edu/voters">NC</a>'
+        $("#stateToVoteIn").append('<div><b>Your vote will be more likely to affect the outcome of the election if you register in North Carolina and vote here. You can find our guide to registering to vote in North Carolina <a href="http://www.dukechronicle.com/article/2016/09/how-to-vote-a-chronicle-guide-to-everything-you-need-to-know">here</a></b></div>');
+    } else {
+        $("#stateToVoteIn").append('Your vote will be more likely to affect the outcome of the election if you vote in the state where you’re currently registered.');
+    }
+
+}
+var election = new Datamap({
+    scope: 'usa',
+    element: document.getElementById('map_election'),
+    geographyConfig: {
+        highlightBorderColor: '#5900b2',
+        popupTemplate: function(geography, data) {
+            return '<div class="hoverinfo"><span class="state">' + geography.properties.name +
+                '</span><br> Tipping Power: <span class="tippingPower">' + data.tippingPower +
+                '</span><br> Voting Power Index: <span class="votingPowerIndex">' + data.votingPowerIndex + '</span>'
+        },
+        highlightBorderWidth: 3
+    },
+
+    fills: colorKey,
+    data: votingData
+});
+election.labels();
