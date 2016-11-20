e702484f688ad1806a8be96c1b0d51ea2c2fcd34
Alexander <xanderyst@gmail.com>
2016-11-20 13:23:06 -0500

    k

--- js/angular-router.js
+++ js/angular-router.js
@@ -74,7 +74,7 @@
 	importSupport.controller('donateController', function($scope, $timeout){
 		$scope.localNeeds = totalNeeds;
     	$scope.localDisaster = disasterType;
-    		// $scope.$apply();
+    		$scope.$apply();
     		$scope.localDisaster = "Hurricane Matthew";
     		console.log($scope.localDisaster);
 
