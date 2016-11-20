4f740e324e1f7d869ce74046cd91387d3df61c20
Eric Song <ess42@duke.edu>
2016-11-20 09:24:20 -0500

    Df

--- js/angular-router.js
+++ js/angular-router.js
@@ -60,7 +60,7 @@
         })
         if($scope.org.food){
         	var len = countyList.length;
-        	$scope.scopeServices = null;
+        	$scope.scopeServices = 5;
         	for(var i =0;i<len;i++){
         		$scope.scopeServices.organizationName = $scope.org.organizationName;
         		$scope.scopeServices.county = countyList[i];
